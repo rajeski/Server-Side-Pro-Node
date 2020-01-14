bda7cc55c3e4ff531b67852ccf563309db632128
rajeski <asiajsr@gmail.com>
2020-01-14 22:16:19 +0900

    Project Updates
    1) Added Mongoose code to index.js file; and, 2) Began Postman testing with Mongoose.

--- movie_api/index.js
+++ movie_api/index.js
@@ -204,8 +204,69 @@ app.get('/year', function(req, res) {
 
 // POST request 
 
-app.post('/user', function(req, res) {
-  res.json(addUser);
+// CREATE Add a User
+/* We'll expect JSON in this format
+{
+  ID : Integer,
+  Username : String, 
+  Password : String, 
+  Email : String, 
+  Birthday : Date
+}*/
+
+app.post('/users', function(req, res) {
+  Users.findOne( {Username : req.body.Username })
+  .then(function(user) {
+    if (user) {
+      return res.status(400).send(req.body.Username + "already exists"); 
+    } else {
+      Users 
+      .create({
+        Username: req.body.Username, 
+        Password: req.body.Password, 
+        Email: req.body.Email, 
+        Birthday: req.body.Birthday  
+      })
+      .then(function(user) { res.status(201).json(user) })
+      .catch(function(error) {
+        console.error(error);
+        res.status(500).send("Error: " + error);
+      })
+    }   
+  }).catch(function(error) {
+    console.error(error);
+    res.status(500).send("Error : + error");
+  });
+});
+
+// app.post('/user', function(req, res) {
+//   res.json(addUser);
+// });
+   
+// READ Get all users
+
+app.get('/users', function(req, res) {
+
+  Users.find()
+  .then(function(users) {
+    res.status(201).json(users)
+  })
+  .catch(function(err) {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
+
+// READ Get a user by username 
+app.get('/users/:Username', function(req, res) {
+  Users.findOne( {Username : req.params.Username })
+  .then(function(users) {
+    res.json(user)
+  })
+  .catch(function(err) {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
 });
 
 app.post('/title', function(req, res) {
@@ -214,11 +275,74 @@ app.post('/title', function(req, res) {
 
 // PUT request 
 
-app.put('/user', function(req, res) {
-  res.json(addUser);
+// UPDATE a user's info, by username
+/* We'll expect JSON in this format
+{
+  Username: String,
+  (required)
+  Password: String, 
+  (required)
+  Email: String,
+  (required)
+  Birthday: Data
+}*/
+
+app.put('/users/:Username', function(req, res) {
+  Users.findOneAndUpdate({ Username : req.params.Username }, { $set :
+  {
+    Username : req.body.Username,
+    Password : req.body.Password,
+    Email : req.body.Email,
+    Birthday : req.body.Birthday
+  }},
+  { new : true }, // This line makes sure that the updated document is returned
+  function(err, updatedUser) {
+    if(err) {
+      console.error(err);
+      res.status(500).send("Error: " +err);
+    } else {
+      res.json(updatedUser)
+    }
+  })
 });
 
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/Movies/:MovieID', function(req, res) {
+  Users.findOneAndUpdate({ Username : req.params.Username }, {
+    $push : { FavoriteMovies : req.params.MovieID }
+  },
+  { new : true }, // This line makes sure that the updated document is returned
+  function(err, updatedUser) {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    } else {
+      res.json(updatedUser)
+    }
+  })
+});
+
+// app.put('/user', function(req, res) {
+//   res.json(addUser);
+// });
+
 // DELETE request 
+
+// Delete a user by username
+app.delete('/users/:Username', function(req, res) {
+  Users.findOneAndRemove({ Username: req.params.Username })
+  .then(function(user) {
+    if (!user) {
+      res.status(400).send(req.params.Username + " was not found");
+    } else {
+      res.status(200).send(req.params.Username + " was deleted.");
+    }
+  })
+  .catch(function(err) {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
 
 app.delete('/title', function(req, res) {
   res.json(removeMovie);
